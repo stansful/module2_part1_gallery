d1ed85d452aa5caae4d457d8d8323f115c189a7b
stansful <stansful@gmail.com>
2022-02-28 18:22:27 +0300

    refactor: move & rename js files
    move all js files to src and rename extension to .ts
    
    feat/MODULE-2-PART-1-gallery

---
+++ src/galleryService.ts
@@ -0,0 +1,55 @@
+const gallery = document.querySelector('#gallery');
+const previousButton = document.querySelector('#previous');
+const nextButton = document.querySelector('#next');
+
+const updateQueryParams = (pageNumber) => {
+  if (location.search !== `?page=${pageNumber}`) {
+    location.search = `?page=${pageNumber}`;
+  }
+};
+
+const nextButtonEvent = async () => {
+  const currentPage = getCurrentPage();
+  const newPage = Number(currentPage) + 1;
+  setNewPage(newPage);
+  await showGallery(getCurrentPage(), getToken());
+};
+
+const previousButtonEvent = async () => {
+  const currentPage = getCurrentPage();
+  const newPage = Number(currentPage) - 1;
+  setNewPage(newPage);
+  await showGallery(getCurrentPage(), getToken());
+};
+
+const showGallery = async (page, token) => {
+  // const expired = checkTokenForExpiration()
+  //
+  // if (expired) {
+  //     redirectToIndex()
+  // }
+
+  updateQueryParams(page);
+
+  const data = await httpGet(`${API_URL}/gallery?page=${page}`, token);
+
+  gallery.innerHTML = '';
+
+  data.objects.forEach((imgLink) => (gallery.innerHTML += `<img src="${imgLink}" width="200" height="200" alt="img">`));
+};
+
+const redirectToIndex = () => {
+  removeToken();
+  nextButton.removeEventListener(EVENT_TYPES.click, nextButtonEvent);
+  previousButton.removeEventListener(EVENT_TYPES.click, previousButtonEvent);
+  return document.location.replace('./index.html');
+};
+
+removeTokenWithTimeout();
+
+validatePage();
+
+showGallery(getCurrentPage(), getToken());
+
+nextButton.addEventListener(EVENT_TYPES.click, nextButtonEvent);
+previousButton.addEventListener(EVENT_TYPES.click, previousButtonEvent);
---
+++ src/tokenService.ts
@@ -0,0 +1,33 @@
+const setToken = (token, timeToLive = TOKEN_TIME_TO_LIVE_IN_MINUTES) => {
+  localStorage.setItem(TOKEN, token);
+  setTokenExpirationTime(timeToLive);
+};
+
+const setTokenExpirationTime = (minutes) => {
+  const expirationTimeInMilliSeconds = minutes * 60 * 1000;
+  localStorage.setItem(TOKEN_EXPIRES_TIME, `${expirationTimeInMilliSeconds}`);
+};
+
+const getTokenExpirationTime = () => {
+  return Number(localStorage.getItem(TOKEN_EXPIRES_TIME));
+};
+
+const getToken = () => {
+  return localStorage.getItem(TOKEN);
+};
+
+const checkTokenForExpiration = () => {
+  return Date.now() > Number(localStorage.getItem(TOKEN_EXPIRES_TIME));
+};
+
+const removeToken = () => {
+  localStorage.removeItem(TOKEN);
+  localStorage.removeItem(TOKEN_EXPIRES_TIME);
+};
+
+const removeTokenWithTimeout = () => {
+  const expiresTime = getTokenExpirationTime();
+  setTimeout(() => {
+    redirectToIndex();
+  }, expiresTime);
+};
